596f9f72f269147f939a3706705748f5c777fb79
meaghan jennings <meaghanljennings@gmail.com>
2020-10-15 21:09:59 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,8 +12,8 @@ class Patient
     @@all
   end
   
-  def new_appointment(date, name)
-    Appointment.new(date, self, name)
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
   end
   
   def appointments
